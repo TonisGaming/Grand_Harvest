a26d2e3d765a4ec40214bbed3f7fb6ec680a50ef
TonisGaming <tonis-tanel@hotmail.com>
2020-08-09 21:43:51 +0300

    Added snipers and a "response time"

--- tonis1/hooks/InteractionTweakData.lua
+++ tonis1/hooks/InteractionTweakData.lua
@@ -1,5 +1,6 @@
-Hooks:PostHook(InteractionTweakData, "init", "tonis1_interactions", function(self, tweak_data)
-	self.pku_ton_thermite = {
+Hooks:PostHook(InteractionTweakData, "init", "tonisl1_interaction", function(self, tweak_data)
+
+	self.gen_pku_thermite = {
         text_id = "pku_ton_thermite",
 		action_text_id = "pku_ton_thermite",
 		sound_start = "bar_bag_generic",
@@ -8,7 +9,8 @@ Hooks:PostHook(InteractionTweakData, "init", "tonis1_interactions", function(sel
 		start_active = false,
 		timer = 3
     }
-	self.pku_ton_intel = {
+	
+	self.gen_prop_loot_confidential_folder_event = {
         text_id = "pku_ton_intel",
 		action_text_id = "pku_ton_intel",
 		sound_start = "bar_bag_generic",
